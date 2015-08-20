bac6bcea5d6e8a994bb853652f0bfe6b11e26202
kitan1982 <anhthao.pham@claroline.net>
2015-08-20 15:43:18 +0200

    Add elearning catalog

--- Controller/RemoteCursusController.php
+++ Controller/RemoteCursusController.php
@@ -108,4 +108,124 @@ class RemoteCursusController extends Controller
 
         return new JsonResponse($datas, 200);
     }
+
+
+    /**
+     * @EXT\Route(
+     *     "/elearning/catalog",
+     *     name="formalibre_claroline_com_theme_elearning_catalog",
+     *     options={"expose"=true}
+     * )
+     * @EXT\Template()
+     */
+    public function eLearningCatalogAction()
+    {
+        $courses = $this->cursusApiManager->getRemoteCourses($this->campusName);
+        $sortedCourses = array();
+
+        foreach ($courses as $course) {
+            $code = $course['code'];
+
+            switch ($code) {
+                case 'blended_learning':
+                    $sortedCourses['blended_learning'] = $course;
+                    break;
+                case 'blog':
+                    $sortedCourses['blog'] = $course;
+                    break;
+                case 'account':
+                    $sortedCourses['account'] = $course;
+                    break;
+                case 'ws_rights':
+                    $sortedCourses['ws_rights'] = $course;
+                    break;
+                case 'resources_rights':
+                    $sortedCourses['resources_rights'] = $course;
+                    break;
+                case 'environment':
+                    $sortedCourses['environment'] = $course;
+                    break;
+                case 'teams_badges':
+                    $sortedCourses['teams_badges'] = $course;
+                    break;
+                case 'ws_teams_badges':
+                    $sortedCourses['ws_teams_badges'] = $course;
+                    break;
+                case 'resources_teams_badges':
+                    $sortedCourses['resources_teams_badges'] = $course;
+                    break;
+                case 'ws_config':
+                    $sortedCourses['ws_config'] = $course;
+                    break;
+                case 'ws_roles_users_management':
+                    $sortedCourses['ws_roles_users_management'] = $course;
+                    break;
+                case 'ws_rights_management':
+                    $sortedCourses['ws_rights_management'] = $course;
+                    break;
+                case 'personal_ws':
+                    $sortedCourses['personal_ws'] = $course;
+                    break;
+                case 'online_evaluation':
+                    $sortedCourses['online_evaluation'] = $course;
+                    break;
+                case 'exercises':
+                    $sortedCourses['exercises'] = $course;
+                    break;
+                case 'forum':
+                    $sortedCourses['forum'] = $course;
+                    break;
+                case 'social_medias':
+                    $sortedCourses['social_medias'] = $course;
+                    break;
+                case 'tools':
+                    $sortedCourses['tools'] = $course;
+                    break;
+                case 'home_page':
+                    $sortedCourses['home_page'] = $course;
+                    break;
+                case 'platform_config':
+                    $sortedCourses['platform_config'] = $course;
+                    break;
+                case 'platform_roles_users_management':
+                    $sortedCourses['platform_roles_users_management'] = $course;
+                    break;
+                case 'screenwriting':
+                    $sortedCourses['screenwriting'] = $course;
+                    break;
+                case 'portfolio':
+                    $sortedCourses['portfolio'] = $course;
+                    break;
+                case 'resources_arrangement':
+                    $sortedCourses['resources_arrangement'] = $course;
+                    break;
+                case 'platform_roles':
+                    $sortedCourses['platform_roles'] = $course;
+                    break;
+                case 'ws_roles':
+                    $sortedCourses['ws_roles'] = $course;
+                    break;
+                case 'website':
+                    $sortedCourses['website'] = $course;
+                    break;
+                case 'survey':
+                    $sortedCourses['survey'] = $course;
+                    break;
+                case 'tutoring':
+                    $sortedCourses['tutoring'] = $course;
+                    break;
+                case 'platform_users':
+                    $sortedCourses['platform_users'] = $course;
+                    break;
+                case 'ws_users':
+                    $sortedCourses['ws_users'] = $course;
+                    break;
+                case 'wiki':
+                    $sortedCourses['wiki'] = $course;
+                    break;
+            }
+        }
+
+        return array('courses' => $courses, 'sortedCourses' => $sortedCourses);
+    }
 }
